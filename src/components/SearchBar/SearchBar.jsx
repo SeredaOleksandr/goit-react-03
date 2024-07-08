b870991c9ec2de7f79fa43c9093d70416f068574
SeredaOleksandr <154976460+SeredaOleksandr@users.noreply.github.com>
2024-07-08 14:59:57 +0300

    Update SearchBar.jsx

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,9 +2,14 @@ import React, { useState } from 'react';
 
 const SearchBar = () => {
   const [inputValue, setInputValue] = useState('');
+
+  const handleChange = evt => {
+    setInputValue(evt.target.value);
+  };
+
   return (
     <div>
-      <input type="text" />
+      <input type="text" value={inputValue} onChange={handleChange} />
       <p>{inputValue}</p>
     </div>
   );
